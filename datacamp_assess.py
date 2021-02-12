0edeca3bdd248f60d7bbb2fd4fc72ea65faf3b9e
JustinAnthonyB <justin.baptiste@gmail.com>
2021-02-12 15:14:52 -0500

    data-assessment-2

--- datacamp_assess.py
+++ datacamp_assess.py
@@ -1,3 +1,9 @@
+'''
+This is my Datacamp assessment questions that I got wrong
+Broken into: 2 set
+'''
+
+#Set 1
 #1
 with open('hello.txt', 'w') as file:
   file.write("hello!")
@@ -48,3 +54,32 @@ c = Color('#00ff66')
 
 c.rgb_value
 
+# Set 2
+def hours_to_seconds(hours):
+    return hours * 60 * 60
+
+hours_to_seconds(8)
+
+
+car = {
+    'brand': 'Ferrari',
+    'model': 'model',
+    'year': 2019
+}
+
+type(car)
+
+
+[s.upper() for s in ['hello', 'world']]
+
+
+[i for i in range(5) if i > 2]
+
+class Planet:
+    def __init__(self, name, diameter_km):
+        self.name = name
+        self.diameter_km = diameter_km
+        
+e = Planet("Earth", 12742)
+
+e.name, e.diameter_km
